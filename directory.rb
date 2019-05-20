2a9e7d27de169994360f41d1c1b0cb90b81d5d4c
James Hylins <james.hylins@johnlewis.co.uk>
2019-05-20 13:40:31 +0000

    refactored to wrap code in methods

--- directory.rb
+++ directory.rb
@@ -11,11 +11,22 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-# and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-# finally, we print the total
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |student|
+    puts student
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
