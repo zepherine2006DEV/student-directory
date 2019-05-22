7a533671c9ce94fa55b3e6ae1e61bf47d241ccdb
James Hylins <james.hylins@johnlewis.co.uk>
2019-05-22 11:34:22 +0000

    using a global variable for students

--- directory.rb
+++ directory.rb
@@ -1,20 +1,19 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   #create an empty array
-  students = []
   #get the first name
   name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     #get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
 end
 
 def print_header
@@ -22,39 +21,47 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 def interactive_menu
-  students = []
+
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
+    print_menu
     # 2. read the input and save it into a variable
     selection = gets.chomp
     # 3. do what the user has asked
     case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
     end
   end
+end
+
+def print_menu
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_students
+  print_footer
 end
 
 interactive_menu
