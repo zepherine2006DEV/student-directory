c80582dfe3bd8a9d5b434545d56f1614a7fa6b6a
James Hylins <james.hylins@johnlewis.co.uk>
2019-05-22 14:45:32 +0000

    added option to load students from csv file

--- directory.rb
+++ directory.rb
@@ -44,6 +44,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -61,6 +62,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -76,6 +79,15 @@ def save_students
     student_data = [student[:name], student[:cohort]] #create a simple 2 element array based on the student hash
     csv_line = student_data.join(",") #create a string from the array, with the elements joined with a comma
     file.puts csv_line #write the string to the file reference
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r") #open the file read-only
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
