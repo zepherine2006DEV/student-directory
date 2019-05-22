b3cfc4402e103f4779d0d751371903efe157c4c8
James Hylins <james.hylins@johnlewis.co.uk>
2019-05-22 09:24:08 +0000

    remove return characters using chop and delete

--- directory-ex8.rb
+++ directory-ex8.rb
@@ -5,14 +5,14 @@ def input_students
   #create an empty array
   students = []
   #get the first name
-  name = gets.chomp
+  name = gets.chop
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
     students << {name: name, cohort: DEFAULT_COHORT}
     puts "Now we have #{students.count} students"
     #get another name from the user
-    name = gets.chomp
+    name = gets.delete("\n")
   end
   students.each do |student|
     user_entry = ""
