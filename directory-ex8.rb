d011260c3357749c7479c2bac2e49cbd9ff6dda1
James Hylins <james.hylins@johnlewis.co.uk>
2019-05-22 08:10:51 +0000

    student cohort can now be entered by user, rather than hard coded

--- directory-ex8.rb
+++ directory-ex8.rb
@@ -1,3 +1,4 @@
+DEFAULT_COHORT = :january
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -8,10 +9,45 @@ def input_students
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: DEFAULT_COHORT}
     puts "Now we have #{students.count} students"
     #get another name from the user
     name = gets.chomp
+  end
+  students.each do |student|
+    user_entry = ""
+    puts "Please enter cohort for #{student[:name]}. Enter to default."
+    user_entry = gets.chomp
+    if user_entry != ""
+      case user_entry
+      when "january"
+        student[:cohort] = :january
+      when "february"
+        student[:cohort] = :february
+      when "march"
+        student[:cohort] = :march
+      when "april"
+        student[:cohort] = :april
+      when "may"
+        student[:cohort] = :may
+      when "june"
+        student[:cohort] = :june
+      when "july"
+        student[:cohort] = :july
+      when "august"
+        student[:cohort] = :august
+      when "september"
+        student[:cohort] = :september
+      when "october"
+        student[:cohort] = :october
+      when "november"
+        student[:cohort] = :november
+      when "december"
+        student[:cohort] = :december
+      else
+        puts "not a valid entry! Default will be used."
+      end
+    end  
   end
   students.each do |student|
     puts "Please enter height of #{student[:name]}"
