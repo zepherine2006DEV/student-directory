e4627c3ad99e06dff510e907699d6bfeeefc2bd4
James Hylins <james.hylins@johnlewis.co.uk>
2019-05-23 12:39:29 +0000

    refactoring and added feedback messages

--- directory.rb
+++ directory.rb
@@ -3,17 +3,15 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
-  #get the first name
+  get_names
+end
+
+def get_names
   name = STDIN.gets.chomp
-  #while the name is not empty, repeat this code
   while !name.empty? do
-    #add the student hash to the array
     add_student(name, :november)
-  #  @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    #get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -22,7 +20,7 @@ def print_header
   puts "-------------"
 end
 
-def print_students
+def print_student_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
@@ -41,17 +39,16 @@ def interactive_menu
 end
 
 def print_menu
-  # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items
+  puts "9. Exit"
 end
 
 def show_students
   print_header
-  print_students
+  print_student_list
   print_footer
 end
 
@@ -73,15 +70,14 @@ def process(selection)
 end
 
 def save_students
-  # open file
   file = File.open("students.csv", "w")
-  # interate over @students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]] #create a simple 2 element array based on the student hash
     csv_line = student_data.join(",") #create a string from the array, with the elements joined with a comma
     file.puts csv_line #write the string to the file reference
   end
   file.close
+  puts "Students successfully saved to file."
 end
 
 def load_students(filename = "students.csv")
@@ -89,9 +85,9 @@ def load_students(filename = "students.csv")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     add_student(name, cohort.to_sym)
-#    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+  puts "Students successfully loading from file."
 end
 
 def try_load_students
