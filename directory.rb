213b5303233b640813013ee69c45e848d63fe422
James Hylins <james.hylins@johnlewis.co.uk>
2019-05-23 07:35:08 +0000

    read file from argument supplied by command line if given

--- directory.rb
+++ directory.rb
@@ -5,7 +5,7 @@ def input_students
   puts "To finish, just hit return twice"
   #create an empty array
   #get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
@@ -35,7 +35,7 @@ def interactive_menu
 
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -83,8 +83,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r") #open the file read-only
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r") #open the file read-only
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -92,4 +92,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first #first arg on command line
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{filename} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end  
+
+try_load_students
 interactive_menu
